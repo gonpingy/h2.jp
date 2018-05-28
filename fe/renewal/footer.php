b2402accf87353982e056fbccf527544736e544a
Hiroyuki Abe <gonpingy@gmail.com>
2018-05-28 20:08:18 +0900

    ティザー用

---
+++ fe/renewal/footer.php
@@ -0,0 +1,44 @@
+      <footer><p class="copyrights"><small>© <?= date('Y') ?> HandiHouse project All Rights Reserved.</small></p></footer>
+    </div>
+
+    <script src="<?= DIR_JS ?>/parallax-background.min.js" ></script>
+    <script type="text/javascript" src="<?= DIR_JS ?>/jquery.inview.js"></script>
+    <script>
+    $(function() {
+      $('.cbefore').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+        if(isInView){
+          $(this).stop().addClass('cshow');
+        }
+        else{
+        //  $(this).stop().removeClass('cshow');
+        }
+      });
+    });
+    $(function() {
+      $('.cbefore2').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+          $(this).stop().addClass('cshow2');
+      });
+    });
+    </script>
+    <script>
+      //クリックで表示
+    $(function(){
+      $('a[href^="#"]').click(function(){
+        var speed = 1500;
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({scrollTop:position}, speed, "swing");
+        return false;
+      });
+    });
+    </script>
+    <script>
+    $(function() {
+    	$(window).on('load resize', function() {
+    		$('.parallax').parallaxBackground();
+    	});
+    });
+    </script>
+  </body>
+</html>
---
+++ fe/renewal/header.php
@@ -0,0 +1,37 @@
+<?php
+require_once(get_template_directory() . '/config.php');
+
+// パスワード保護
+if (post_password_required($post)) {
+  echo get_the_password_form();
+  exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>HandiHouse project</title>
+
+   <meta name="description" content="HandiHouseproject、第2章が始まります。">
+   <meta property="og:url" content="<?= get_current_link(); ?>">
+   <meta property="og:type" content="website">
+   <meta property="og:title" content="HandiHouseproject will be REBORN">
+   <meta property="og:site_name" content="HandiHouseproject">
+   <meta property="og:description" content="HandiHouseproject、第2章が始まります。">
+   <meta property="og:image" content="<?= DIR_IMG ?>/ogp.png">
+   <meta property="og:locale" content="ja_JP">
+    <link rel="icon" href="<?= DIR_IMG ?>/favicon.ico">
+    <!-- jQuery読み込み -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- CSS読み込み -->
+    <link rel="stylesheet" href="<?= DIR_CSS ?>/normalize.css">
+    <link rel="stylesheet" href="<?= DIR_CSS ?>/hhstyle.css">
+    <link rel="stylesheet" href="<?= DIR_CSS ?>/para_bg.css" />
+
+    <?php wp_head(); ?>
+  </head>
+  <body>
