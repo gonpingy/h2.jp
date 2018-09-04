b28062f04eaaf02bee16480eb33a214fcce1480a
Hiroyuki Abe <gonpingy@gmail.com>
2018-09-04 23:33:43 +0900

    cp detail

---
+++ fe/renewal/cp_detail.php
@@ -0,0 +1,94 @@
+<?php
+/*
+ * Template Name: COLLABORATION PROJECTS 詳細
+ */
+get_header();
+?>
+
+  <div id="hhwrapper">
+    <div id="header">
+      <h1><a href="index.html"><img src="<?= DIR_IMG ?>/hh_logo.png" alt="HandiHouse"></a></h1>
+      <ul id="g_nav" class="pc">
+        <li><a href="vision.html">VISION</a></li>
+        <li><a href="project.html">PROJECT</a></li>
+        <li><a href="media.html">MEDIA</a></li>
+        <li><a href="company.html">COMPANY</a></li>
+        <li><a href="contact.html"><img src="<?= DIR_IMG ?>/i_contact.png" alt="CONTACT"></a></li>
+        <li><a href="https://www.facebook.com/HandiHouseproject/" target="_blank"><img src="<?= DIR_IMG ?>/i_fb.png" alt="HandiHouse facebook"></a></li>
+        <li><a href="https://www.instagram.com/handihouseproject/" target="_blank"><img src="<?= DIR_IMG ?>/i_insta.png" alt="HandiHouse instagram"></a></li>
+      </ul>
+      <div class="nav_drawer sp">
+        <input id="nav_input" type="checkbox" class="nav_unshown">
+        <label class="nav_open" for="nav_input"><span></span><span></span><span></span></label>
+        <div id="nav_content">
+          <ul>
+            <li><a href="vision.html">VISION</a></li>
+            <li><a href="project.html">PROJECT</a></li>
+            <li><a href="project_c_top.html">- COLLABORATION PROJECT</a></li>
+            <li><a href="project_i_top.html">- INDEPENDENT PROJECT</a></li>
+            <li><a href="media.html">MEDIA</a></li>
+            <li><a href="company.html">COMPANY</a></li>
+            <li><a href="contact.html"><img src="<?= DIR_IMG ?>/i_contact_w.png" alt="CONTACT"></a></li>
+            <li><a href="https://www.facebook.com/HandiHouseproject/" target="_blank"><img src="<?= DIR_IMG ?>/i_fb_w.png" alt="HandiHouse facebook"></a>
+              <a href="https://www.instagram.com/handihouseproject/" target="_blank"><img src="<?= DIR_IMG ?>/i_insta_w.png" alt="HandiHouse instagram"></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div id="pan">
+      <a href="project.html">PROJECT</a>　>　<a href="project_c_top.html">COLLABORATION PROJECTS</a> > トキハウス
+    </div>
+    <div class="contents_wrapper">
+      <div id="cp_detail" class="contents_area">
+        <div class="date">2018.07.11</div>
+        <h3>トキハウス</h3>
+        <div id="copy">施主こだわりの壁紙＋塗装壁。昭和レトロを現代に</div>
+        <div id="cp_member">担当：中田・山崎</div>
+        <div id="article">
+          <img src="<?= DIR_IMG ?>/cd01.jpg">
+          <p>説明文が入ります説明文が入ります説明文が入ります説明文が入ります説明文が入ります。<br>説明文が入ります説明文が入ります説明文が入ります説明文が入ります。<br>説明文が入ります説明文が入ります説明文が入ります説明文が入ります説明文が入ります。</p>
+          <hr>
+          <img src="<?= DIR_IMG ?>/cd02.jpg">
+          <p>説明文が入ります説明文が入ります説明文が入ります説明文が入ります説明文が入ります。</p>
+          <hr>
+          <img src="<?= DIR_IMG ?>/cd03.jpg">
+          <p>説明文が入ります説明文が入ります説明文が入ります説明文が入ります説明文が入ります。</p>
+          <hr>
+          <img src="<?= DIR_IMG ?>/cd04.jpg">
+          <p>説明文が入ります説明文が入ります説明文が入ります説明文が入ります説明文が入ります。</p>
+          <hr>
+          <img src="<?= DIR_IMG ?>/cd05.jpg">
+          <p>説明文が入ります説明文が入ります説明文が入ります説明文が入ります説明文が入ります。</p>
+          <hr>
+          <img src="<?= DIR_IMG ?>/cd06.jpg"><br>
+          <img src="<?= DIR_IMG ?>/cd07.jpg"><br>
+          <img src="<?= DIR_IMG ?>/cd08.jpg">
+        </div>
+        <div id="cp_info">
+          <h3>ABOUT PROJECT</h3>
+          <div id="cp_info_d">
+            <p>設計　　　加藤渓一 / studioPEACEsign<br> 施工　　　HandiHouse project<br> 期間　　　2017.12-2018.04
+              <br> 場所　　　東京都八王子市
+              <br> 用途　　　1F店舗 2F住宅<br> 面積　　　30.0㎡
+            </p>
+            <p><a href="">プロジェクトのアルバムを見る（Facebook） ></a>
+          </div>
+        </div>
+        <ul id="cp_detail_nav">
+          <li class=""><a href="">＜ PREV</a></li>
+          <li><a href="project_c_top.html">一覧へ</a></li>
+          <li class=""><a href="">NEXT ></a></li>
+        </ul>
+      </div>
+
+      <div class="op">
+        <h2>OUR PROJECT</h2></div>
+      <ul id="op_nav">
+        <li><a href="https://www.facebook.com/handilabo" target="_blank"><img src="<?= DIR_IMG ?>/op01.jpg" class="pc" alt="HANDI LABO"><img src="<?= DIR_IMG ?>/op01_sp.jpg" class="sp" alt="HANDI LABO"></a></li>
+        <li><a href="https://seasideliving-zushi.com/" target="_blank"><img src="<?= DIR_IMG ?>/op02.jpg" class="pc" alt="SEASIDE LIVING"><img src="<?= DIR_IMG ?>/op02_sp.jpg" class="sp" alt="SEASIDE LIVING"></a></li>
+        <li><a href="https://www.facebook.com/Apart.kitano.47/" target="_blank"><img src="<?= DIR_IMG ?>/op03.jpg" class="pc" alt="DIY APARTMENT"><img src="<?= DIR_IMG ?>/op03_sp.jpg" class="sp" alt="DIY APARTMENT"></a></li>
+        <li><a href="https://www.facebook.com/zushinonose/" target="_blank"><img src="<?= DIR_IMG ?>/op04.jpg" class="pc" alt="ZUSHI NONOSE"><img src="<?= DIR_IMG ?>/op04_sp.jpg" class="sp" alt="ZUSHI VILLAGE"></a></li>
+      </ul>
+    </div>
+
+<?php get_footer(); ?>
