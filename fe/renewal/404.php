15b36e6f63d6fcda31a9af7d0d416c1b3e7396ae
Hiroyuki Abe <gonpingy@gmail.com>
2018-09-05 21:40:25 +0900

    404

---
+++ fe/renewal/404.php
@@ -0,0 +1,131 @@
+<?php
+/*
+ * Template Name: 404 NOT FOUND
+ */
+get_header();
+?>
+
+  <div id="hhwrapper">
+    <div id="header">
+      <h1><a href="index.html"><img src="<?= DIR_IMG ?>/hh_logo.png" alt="HandiHouse"></a></h1>
+      <ul id="g_nav" class="pc">
+        <li><a href="vision.html">VISION</a></li>
+        <li><a href="project.html">PROJECT</a></li>
+        <li><a href="media.html">MEDIA</a></li>
+        <li><a href="company.html">COMPANY</a></li>
+        <li><a href="contact.html"><img src="<?= DIR_IMG ?>/i_contact.png" alt="CONTACT"></a></li>
+        <li><a href="https://www.facebook.com/HandiHouseproject/" target="_blank"><img src="<?= DIR_IMG ?>/i_fb.png" alt="HandiHouse facebook"></a></li>
+        <li><a href="https://www.instagram.com/handihouseproject/" target="_blank"><img src="<?= DIR_IMG ?>/i_insta.png" alt="HandiHouse instagram"></a></li>
+      </ul>
+      <div class="nav_drawer sp">
+        <input id="nav_input" type="checkbox" class="nav_unshown">
+        <label class="nav_open sp" for="nav_input"><span></span><span></span><span></span></label>
+        <div id="nav_content">
+          <ul>
+            <li><a href="vision.html">VISION</a></li>
+            <li><a href="project.html">PROJECT</a></li>
+            <li><a href="project_c_top.html">- COLLABORATION PROJECT</a></li>
+            <li><a href="project_i_top.html">- INDEPENDENT PROJECT</a></li>
+            <li><a href="media.html">MEDIA</a></li>
+            <li><a href="company.html">COMPANY</a></li>
+            <li><a href="contact.html"><img src="<?= DIR_IMG ?>/i_contact_w.png" alt="CONTACT"></a></li>
+            <li><a href="https://www.facebook.com/HandiHouseproject/" target="_blank"><img src="<?= DIR_IMG ?>/i_fb_w.png" alt="HandiHouse facebook"></a> <a href="https://www.instagram.com/handihouseproject/" target="_blank"><img src="<?= DIR_IMG ?>/i_insta_w.png" alt="HandiHouse instagram"></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div id="pan">
+      <a href="index.html">HOME</a>　>　404 NOT FOUND
+    </div>
+
+    <div class="contents_wrapper">
+      <div id="nf_notice" class="contents_area">
+      <h2>404 NOT FOUND</h2>
+      <p>
+        お探しのページは存在しません。<br>
+        <a href="index.html" style="text-decoration:underline;">HandiHouse project トップページへ移動する</a>
+      </p>
+    </div>
+    <!--  <div id="media" class="contents_area">
+        <h2>MEDIA</h2>
+        <ul id="genre_nav">
+          <li class="selected"><a href="">ALL</a></li>
+          <li><a href="">MAGAZINE</a></li>
+          <li><a href="">WEB</a></li>
+          <li><a href="">AWARD</a></li>
+          <li><a href="">LECTURE</a></li>
+        </ul>
+
+        <div class="media_content">
+          <div class="content_info"><span class="date">2018.06.15</span> <span class="genre">MAGAZINE</span></div>
+          <h3>掲載誌のお知らせ</h3>
+          <p>マガジンハウスが刊行する「アンドプレミアム」に宮崎台のヴィンテージマンション１が掲載されました！<br>完成から６年経ってからもこうして取り上げ頂けて嬉しい限り！<br>是非ご覧ください。</p>
+        </div>
+        <div class="media_content">
+          <div class="content_info"><span class="date">2018.06.10</span> <span class="genre">AWARD</span></div>
+          <h3>これからの建築士賞　受賞</h3>
+          <p>断熱タイニーハウスプロジェクトが東京建築士会の第4回これからの建築士賞を受賞致しました！<br>体感を届ける旅は、まだまだこれからです！ますます精進していきます！<br>沼田汐里<br>森本天希<br>中田理恵（HandiHouse project）</p>
+        </div>
+        <div class="media_content">
+          <div class="content_info"><span class="date">2018.05.24</span> <span class="genre">WEB</span></div>
+          <h3>Webマガジン掲載のお知らせ</h3>
+          <p>日野の家が戸建リノベwebマガジンの100%LIFEに掲載されました！<br>ぜひご覧ください〜！<br><a href="https://100life.jp/do-it-yourself/34484/" target="_blank">https://100life.jp/do-it-yourself/34484/</a></p>
+        </div>
+        <div class="media_content">
+          <div class="content_info"><span class="date">2018.05.24</span> <span class="genre">WEB</span></div>
+          <h3>Webマガジン掲載のお知らせ</h3>
+          <p>昨年の夏に作りました「中目黒の家」がカウカモマガジンに掲載されました。<br>お施主さんと話す中で「食を大事にしたい」という想いを形にした、大きなカウンターキッチン。<br><a href="https://cowcamo.jp/magazine/column/キッチンが中心の家" target="_blank">https://cowcamo.jp/magazine/column/キッチンが中心の家</a></p>
+        </div>
+        <div class="media_content">
+          <div class="content_info"><span class="date">2018.06.15</span> <span class="genre">MAGAZINE</span></div>
+          <h3>掲載誌のお知らせ</h3>
+          <p>マガジンハウスが刊行する「アンドプレミアム」に宮崎台のヴィンテージマンション１が掲載されました！<br>完成から６年経ってからもこうして取り上げ頂けて嬉しい限り！<br>是非ご覧ください。</p>
+        </div>
+        <div class="media_content">
+          <div class="content_info"><span class="date">2018.06.10</span> <span class="genre">AWARD</span></div>
+          <h3>これからの建築士賞　受賞</h3>
+          <p>断熱タイニーハウスプロジェクトが東京建築士会の第4回これからの建築士賞を受賞致しました！<br>体感を届ける旅は、まだまだこれからです！ますます精進していきます！<br>沼田汐里<br>森本天希<br>中田理恵（HandiHouse project）</p>
+        </div>
+        <div class="media_content">
+          <div class="content_info"><span class="date">2018.05.24</span> <span class="genre">WEB</span></div>
+          <h3>Webマガジン掲載のお知らせ</h3>
+          <p>日野の家が戸建リノベwebマガジンの100%LIFEに掲載されました！<br>ぜひご覧ください〜！<br><a href="https://100life.jp/do-it-yourself/34484/" target="_blank">https://100life.jp/do-it-yourself/34484/</a></p>
+        </div>
+        <div class="media_content">
+          <div class="content_info"><span class="date">2018.05.24</span> <span class="genre">WEB</span></div>
+          <h3>Webマガジン掲載のお知らせ</h3>
+          <p>昨年の夏に作りました「中目黒の家」がカウカモマガジンに掲載されました。<br>お施主さんと話す中で「食を大事にしたい」という想いを形にした、大きなカウンターキッチン。<br><a href="https://cowcamo.jp/magazine/column/キッチンが中心の家" target="_blank">https://cowcamo.jp/magazine/column/キッチンが中心の家</a></p>
+        </div>
+        <div class="media_content">
+          <div class="content_info"><span class="date">2018.06.15</span> <span class="genre">MAGAZINE</span></div>
+          <h3>掲載誌のお知らせ</h3>
+          <p>マガジンハウスが刊行する「アンドプレミアム」に宮崎台のヴィンテージマンション１が掲載されました！<br>完成から６年経ってからもこうして取り上げ頂けて嬉しい限り！<br>是非ご覧ください。</p>
+        </div>
+        <div class="media_content">
+          <div class="content_info"><span class="date">2018.06.10</span> <span class="genre">AWARD</span></div>
+          <h3>これからの建築士賞　受賞</h3>
+          <p>断熱タイニーハウスプロジェクトが東京建築士会の第4回これからの建築士賞を受賞致しました！<br>体感を届ける旅は、まだまだこれからです！ますます精進していきます！<br>沼田汐里<br>森本天希<br>中田理恵（HandiHouse project）</p>
+        </div>
+        <ul id="page_nav">
+          <li><a href=""><img src="<?= DIR_IMG ?>/page_p.png"alt="prev"></a></li>
+          <li class="current"><a href="">1</a></li>
+          <li><a href="">2</a></li>
+          <li><a href="">3</a></li>
+          <li><a href="">4</a></li>
+          <li><a href="">5</a></li>
+          <li><a href=""><img src="<?= DIR_IMG ?>/page_n.png"alt="next"></a></li>
+        </ul>
+      -->
+      </div>
+
+      <div class="op">
+        <h2>OUR PROJECT</h2></div>
+      <ul id="op_nav">
+        <li><a href="https://www.facebook.com/handilabo" target="_blank"><img src="<?= DIR_IMG ?>/op01.jpg" class="pc" alt="HANDI LABO"><img src="<?= DIR_IMG ?>/op01_sp.jpg" class="sp" alt="HANDI LABO"></a></li>
+        <li><a href="https://seasideliving-zushi.com/" target="_blank"><img src="<?= DIR_IMG ?>/op02.jpg" class="pc" alt="SEASIDE LIVING"><img src="<?= DIR_IMG ?>/op02_sp.jpg" class="sp" alt="SEASIDE LIVING"></a></li>
+        <li><a href="https://www.facebook.com/Apart.kitano.47/" target="_blank"><img src="<?= DIR_IMG ?>/op03.jpg" class="pc" alt="DIY APARTMENT"><img src="<?= DIR_IMG ?>/op03_sp.jpg" class="sp" alt="DIY APARTMENT"></a></li>
+        <li><a href="https://www.facebook.com/zushinonose/" target="_blank"><img src="<?= DIR_IMG ?>/op04.jpg" class="pc" alt="ZUSHI NONOSE"><img src="<?= DIR_IMG ?>/op04_sp.jpg" class="sp" alt="ZUSHI VILLAGE"></a></li>
+      </ul>
+    </div>
+
+<?php get_footer(); ?>
