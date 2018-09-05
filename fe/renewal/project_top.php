c45e20c3eef3e696fc2b795257a83f908ecebf37
Hiroyuki Abe <gonpingy@gmail.com>
2018-09-05 22:15:37 +0900

    our project

--- fe/renewal/project_top.php
+++ fe/renewal/project_top.php
@@ -43,17 +43,6 @@ get_header();
     </div>
     <div class="contents_wrapper">
 
-      <div class="op">
-        <h2>OUR PROJECT</h2></div>
-      <ul id="op_nav">
-        <li><a href="https://www.facebook.com/handilabo" target="_blank"><img src="<?= DIR_IMG ?>/op01.jpg" class="pc" alt="HANDI LABO"><img src="<?= DIR_IMG ?>/op01_sp.jpg" class="sp" alt="HANDI LABO"></a></li>
-        <li><a href="https://seasideliving-zushi.com/" target="_blank"><img src="<?= DIR_IMG ?>/op02.jpg" class="pc" alt="SEASIDE LIVING"><img src="<?= DIR_IMG ?>/op02_sp.jpg" class="sp" alt="SEASIDE LIVING"></a></li>
-        <li><a href="https://www.facebook.com/Apart.kitano.47/" target="_blank"><img src="<?= DIR_IMG ?>/op03.jpg" class="pc" alt="DIY APARTMENT"><img src="<?= DIR_IMG ?>/op03_sp.jpg" class="sp" alt="DIY APARTMENT"></a></li>
-        <li><a href="https://www.facebook.com/zushinonose/" target="_blank"><img src="<?= DIR_IMG ?>/op04.jpg" class="pc" alt="ZUSHI NONOSE"><img src="<?= DIR_IMG ?>/op04_sp.jpg" class="sp" alt="ZUSHI VILLAGE"></a></li>
-      </ul>
-    </div>
+<?php get_template_part('2018/module/our_project'); ?>
 
-<?php
-// フッタ表示
-get_footer();
-?>
+<?php get_footer(); ?>
