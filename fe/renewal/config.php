f8378981b4b1a98a50bb040eea6a6e636bb2ced5
Hiroyuki Abe <gonpingy@gmail.com>
2018-09-12 21:29:41 +0900

    independent project

--- fe/renewal/config.php
+++ fe/renewal/config.php
@@ -6,3 +6,5 @@ define('DIR_JS', get_template_directory_uri() . '/2018/js');
 define('DIR_CSS_TEASER', get_template_directory_uri() . '/teaser/css');
 define('DIR_IMG_TEASER', get_template_directory_uri() . '/teaser/img');
 define('DIR_JS_TEASER', get_template_directory_uri() . '/teaser/js');
+var_dump(site_url());
+define('URL_VISION', get_template_directory_uri() . '/teaser/css');
