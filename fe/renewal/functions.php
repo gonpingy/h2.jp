90dc6c3902860bb217a9c76dadafbaedf21ecd87
Hiroyuki Abe <gonpingy@gmail.com>
2018-09-16 13:26:56 +0900

    media

--- fe/renewal/functions.php
+++ fe/renewal/functions.php
@@ -12,3 +12,43 @@ add_theme_support( 'post-thumbnails' );
 function get_current_link() {
  return (is_ssl() ? 'https' : 'http') . '://' . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
 }
+
+function pagination($pages = '', $range = 2) {
+  $showitems = ($range * 2) + 1;
+
+  global $paged;
+  if(empty($paged)) $paged = 1;
+
+  if($pages == '') {
+    global $wp_query;
+    $pages = $wp_query->max_num_pages;
+
+    if(!$pages) {
+      $pages = 1;
+    }
+  }
+
+  if(1 != $pages) {
+    echo '<ul id="page_nav">';
+
+    if ($paged > 1 && $showitems < $pages) {
+      echo sprintf('<li><a href=""><img src="%s" alt="prev"></a></li>', DIR_IMG . '/page_p.png');
+    }
+
+    for ($i = 1; $i <= $pages; $i++) {
+      if (1 != $pages && (!($i >= $paged + $range + 1 || $i <= $paged - $range - 1) || $pages <= $showitems )) {
+        if ($paged == $i) {
+          echo sprintf('<li class="current"><a href="">%d</a></li>', $i);
+        } else {
+          echo sprintf('<li><a href="">%d</a></li>', $i);
+        }
+      }
+    }
+
+    if ($paged < $pages && $showitems < $pages) {
+      echo sprintf('<li><a href=""><img src="%s" alt="next"></a></li>', DIR_IMG . '/page_n.png');
+    }
+
+    echo '</ul>';
+  }
+}
