0387492871d55da00b4bbf7f8813225cc874b603
Hiroyuki Abe <gonpingy@gmail.com>
2018-09-17 13:59:39 +0900

    cp detail

--- fe/renewal/cp_detail.php
+++ fe/renewal/cp_detail.php
@@ -3,48 +3,43 @@
  * Template Name: COLLABORATION PROJECTS 詳細
  */
 get_header();
+
+$member = [SCF::get('member1')];
+
+if (!empty(SCF::get('member2'))) {
+  $member[] = SCF::get('member2');
+}
+
+$previous = get_previous_page();
+$next = get_next_page();
 ?>
 
     <div class="contents_wrapper">
       <div id="cp_detail" class="contents_area">
-        <div class="date">2018.07.11</div>
-        <h3>トキハウス</h3>
-        <div id="copy">施主こだわりの壁紙＋塗装壁。昭和レトロを現代に</div>
-        <div id="cp_member">担当：中田・山崎</div>
+        <div class="date">?= SCF::get('date') ?></div>
+        <h3><?= get_the_title() ?></h3>
+        <div id="copy"><?= SCF::get('copy') ?></div>
+        <div id="cp_member">担当：<?= implode('・', $member) ?></div>
         <div id="article">
-          <img src="<?= DIR_IMG ?>/cd01.jpg">
-          <p>説明文が入ります説明文が入ります説明文が入ります説明文が入ります説明文が入ります。<br>説明文が入ります説明文が入ります説明文が入ります説明文が入ります。<br>説明文が入ります説明文が入ります説明文が入ります説明文が入ります説明文が入ります。</p>
-          <hr>
-          <img src="<?= DIR_IMG ?>/cd02.jpg">
-          <p>説明文が入ります説明文が入ります説明文が入ります説明文が入ります説明文が入ります。</p>
-          <hr>
-          <img src="<?= DIR_IMG ?>/cd03.jpg">
-          <p>説明文が入ります説明文が入ります説明文が入ります説明文が入ります説明文が入ります。</p>
-          <hr>
-          <img src="<?= DIR_IMG ?>/cd04.jpg">
-          <p>説明文が入ります説明文が入ります説明文が入ります説明文が入ります説明文が入ります。</p>
-          <hr>
-          <img src="<?= DIR_IMG ?>/cd05.jpg">
-          <p>説明文が入ります説明文が入ります説明文が入ります説明文が入ります説明文が入ります。</p>
-          <hr>
-          <img src="<?= DIR_IMG ?>/cd06.jpg"><br>
-          <img src="<?= DIR_IMG ?>/cd07.jpg"><br>
-          <img src="<?= DIR_IMG ?>/cd08.jpg">
+          <?php foreach (SCF::get('本文') as $i => $group): ?>
+          <?= $group['article'] ?>
+          <?php endforeach; ?>
         </div>
         <div id="cp_info">
           <h3>ABOUT PROJECT</h3>
           <div id="cp_info_d">
-            <p>設計　　　加藤渓一 / studioPEACEsign<br> 施工　　　HandiHouse project<br> 期間　　　2017.12-2018.04
-              <br> 場所　　　東京都八王子市
-              <br> 用途　　　1F店舗 2F住宅<br> 面積　　　30.0㎡
-            </p>
-            <p><a href="">プロジェクトのアルバムを見る（Facebook） ></a>
+            <?= SCF::get('about_project') ?>
+            <p><a href="<?= SCF::get('projcet_album') ?>">プロジェクトのアルバムを見る（Facebook） ></a>
           </div>
         </div>
         <ul id="cp_detail_nav">
-          <li class=""><a href="">＜ PREV</a></li>
-          <li><a href="project_c_top.html">一覧へ</a></li>
-          <li class=""><a href="">NEXT ></a></li>
+          <?php if (!empty($previous)): ?>
+          <li class=""><a href="<?= $previous->guid ?>">＜ PREV</a></li>
+          <?php endif; ?>
+          <li><a href="<?= URL_COLLABORATION_PROJECTS ?>">一覧へ</a></li>
+          <?php if (!empty($next)): ?>
+          <li class=""><a href="<?= $next->guid ?>">NEXT ></a></li>
+          <?php endif; ?>
         </ul>
       </div>
 
--- fe/renewal/functions.php
+++ fe/renewal/functions.php
@@ -52,3 +52,105 @@ function pagination($pages = '', $range = 2) {
     echo '</ul>';
   }
 }
+
+function get_next_page() {
+    $get_adjacent_page = new get_adjacent_page();
+    return $get_adjacent_page->get_next_page();
+}
+function get_previous_page() {
+    $get_adjacent_page = new get_adjacent_page();
+    return $get_adjacent_page->get_previous_page();
+}
+
+/**
+ * get_adjacent_page
+ * 前後の固定ページ（同階層）を取得するクラス
+ */
+class get_adjacent_page {
+    private $post = null;
+    private $wpdb = null;
+    private $op = '';
+    private $orderby = '';
+ 
+    /**
+     * __construct
+     */
+    public function __construct() {
+        global $post;
+        global $wpdb;
+        $this->post = $post;
+        $this->wpdb = $wpdb;
+    }
+ 
+    /**
+     * get_next_page
+     * 後の固定ページを取得する
+     * @return  null or $post
+     */
+    public function get_next_page() {
+        $this->op = '>=';
+        $this->orderby = 'ASC';
+        return $this->get_adjacent_post( 'next' );
+    }
+ 
+    /**
+     * get_previous_page
+     * 前の固定ページを取得する
+     * @return  null or $post
+     */
+    public function get_previous_page() {
+        $this->op = '<=';
+        $this->orderby = 'DESC';
+        return $this->get_adjacent_post( 'previous' );
+    }
+ 
+    /**
+     * get_adjacent_post
+     * get_adjacent_post関数を実行
+     * @params  next or previous
+     * @return  null or $post
+     */
+    private function get_adjacent_post( $adjacent ) {
+        if ( !in_array( $adjacent, array( 'previous', 'next' ) ) ) return;
+        $previous = ( $adjacent === 'previous' ) ? true : false;
+        add_filter( 'get_'.$adjacent.'_post_join', array( $this, 'get_page_join' ), 10, 3 );
+        add_filter( 'get_'.$adjacent.'_post_where', array( $this, 'get_page_where' ), 10, 3 );
+        add_filter( 'get_'.$adjacent.'_post_sort', array( $this, 'get_page_sort' ) );
+        return get_adjacent_post( false, '', $previous );
+    }
+ 
+    /**
+     * get_page_join
+     * get_{$adjacent}_post_joinにフックさせる関数
+     * @return  null
+     */
+    public function get_page_join( $join, $in_same_cat, $excluded_categories ) {
+        return;
+    }
+ 
+    /**
+     * get_page_where
+     * get_{$adjacent}_post_whereにフックさせる関数
+     * @return  String  where文
+     */
+    public function get_page_where( $where, $in_same_cat, $excluded_categories ) {
+        $where = $this->wpdb->prepare( "WHERE
+            p.ID != %s AND
+            p.post_parent = %s AND
+            ( p.menu_order $this->op %s OR ( p.menu_order = %s AND p.post_title $this->op %s ) ) AND
+            p.post_type = %s AND
+            p.post_status = 'publish'",
+            $this->post->ID, $this->post->post_parent, $this->post->menu_order, $this->post->menu_order, $this->post->post_title, $this->post->post_type
+        );
+        return $where;
+    }
+
+    /**
+     * get_page_sort
+     * get_{$adjacent}_post_sortにフックさせる関数
+     * @return  String  order by, limit文
+     */
+    public function get_page_sort( $sort ) {
+        return "ORDER BY p.menu_order $this->orderby, p.post_title $this->orderby LIMIT 1";
+    }
+}
