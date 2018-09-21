16415e6545dda6922e027d462e6746a27921b614
Hiroyuki Abe <gonpingy@gmail.com>
2018-09-22 00:15:12 +0900

    mail

--- fe/renewal/contact.php
+++ fe/renewal/contact.php
@@ -25,13 +25,13 @@ get_header();
 
 <p><label for="contact_company">御社名（法人の方）</label><br>[text contact_company id:contact_company size:50]</p>
 
-<p><label for="contact_mail">メールアドレス</label>　<span class="require">必須</span><br>[email contact_mail id:contact_mail size:120]</p>
+<p><label for="contact_mail">メールアドレス</label>　<span class="require">必須</span><br>[email* contact_mail id:contact_mail size:120]</p>
 
 <p><label for="contact_phone">お電話番号</label>　<span class="require">必須</span><br>[text contact_phone id:contact_phone size:50]</p>
 
 <p><label for="contact_text">お問い合わせ内容</label>　<span class="require">必須</span><br><small>※お仕事をご依頼のお客様へ<br>物件情報（住所、面積、構造）やご予算などお分りになる範囲でご記入下さい。</small>[textarea contact_text id:contact_text rows:10 cols:50]
 
-          <p><a href="javascript:contact_form.submit()" class="btn_a nouse">送信</a></p>
+<p><a id="submit" href="javascript:$('form').submit();" class="btn_a nouse">送信</a></p>
          */
         ?>
         <p>
